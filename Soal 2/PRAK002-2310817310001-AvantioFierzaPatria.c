42950dacdacccc85eee939a816080c3ddf53440e
gr1ff0m <114456755+gr1ff0m@users.noreply.github.com>
2023-09-13 12:43:28 +0800

    Add files via upload

---
+++ Soal 2/PRAK002-2310817310001-AvantioFierzaPatria.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    puts("Selamat Pagi, Nama Saya Avantio");
+    puts("Selamat Siang, Nama Saya Avantio");
+    puts("Selamat Malam, Nama Saya Avantio");
+
+    return 0;
+}
---
+++ Soal 2/PRAK002-2310817310001-AvantioFierzaPatria.py
@@ -0,0 +1,3 @@
+print ("Selamat Pagi, Nama Saya Avantio")
+print ("Selamat Siang, Nama Saya Avantio")
+print ("Selamat Malam, Nama Saya Avantio")
