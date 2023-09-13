f658a4f2139c4ecc47af8795e1c0dde9dd1da8b1
gr1ff0m <114456755+gr1ff0m@users.noreply.github.com>
2023-09-13 12:44:37 +0800

    Add files via upload

---
+++ Soal 4/PRAK004-2310817310001-AvantioFierzaPatria.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    puts("#############################");
+    puts("#                           #");
+    puts("#   Avantio Fierza Patria   #");
+    puts("#       23108173100001      #");
+    puts("#                           #");
+    puts("#############################");
+    
+}
