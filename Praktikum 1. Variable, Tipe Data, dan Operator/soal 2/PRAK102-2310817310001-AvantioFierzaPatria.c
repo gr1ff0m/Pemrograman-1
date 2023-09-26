5dbe5d94f9a74dc23cb58dba61e6fde2c0851bc4
gr1ff0m <114456755+gr1ff0m@users.noreply.github.com>
2023-09-26 15:56:16 +0800

    Add files via upload

---
+++ Praktikum 1. Variable, Tipe Data, dan Operator/soal 2/PRAK102-2310817310001-AvantioFierzaPatria.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 4;
+    int b = 8;
+    int c = 3;
+
+    printf("Variabel a bernilai %d\n", a);
+    printf("Variabel b bernilai %d\n", b);
+    printf("Variabel c bernilai %d\n", c);
+
+    float hasil = ((float)a * (float)b) / (float)c;
+
+    printf("Hasil dari a dikali b dibagi c adalah %.6f\n", hasil);
+
+    return 0;
+}
---
+++ Praktikum 1. Variable, Tipe Data, dan Operator/soal 7/PRAK107-2310817310001-AvantioFierzaPatria.py
@@ -0,0 +1,20 @@
+sisi1 = 4
+sisi2 = 5
+sisi3 = 7
+
+
+keliling_tanah = sisi1 + sisi2 + sisi3
+
+
+harga_per_meter = 85000
+
+
+biaya = keliling_tanah * harga_per_meter
+
+
+print("Diketahui :")
+print(f"Panjang sisi segitiga berturut-turut adalah {sisi1}, {sisi2}, dan {sisi3}")
+print(f"Keliling Tanah Pak Dengklek adalah {keliling_tanah}")
+print(f"Harga tanah Per Meter adalah {harga_per_meter}")
+print("\nJawaban :")
+print(f"Biaya yang diperlukan Pak Dengklek adalah : Rp {biaya}")
